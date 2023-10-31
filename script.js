e8fa020151a90f09a042d246e51d726829ea75f1
arskill11 <atrinozenko3@gmail.com>
2023-10-31 22:21:32 +0300

    Added restart option

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 function Player (playerName, marker) {
-    const choice  = document.querySelector('.choose');
-
     return {playerName, marker}
 }
 
-const game =  (() => {
+const game = (() => {
+    const winnerHeading = document.querySelector('.winHead');
+    const player = document.querySelector('.player');
+    const playerMark = document.querySelector('.mark');
 
-    
+    let playerOne;
+    let playerTwo;
+    let currentPlayer;
+    let freeCells;
+    let winnerClaimed;
 
-    const playerOne = Player('Player One', 'X');
-    const playerTwo = Player('Player Two', 'O');
+    function start () {
+        playerOne = Player ('Player One', 'X');
+        playerTwo = Player ('Player Two', 'O');
 
-    let currentPlayer = playerOne;
-    let freeCells = 9;
-    let winnerClaimed = false;
+        currentPlayer = playerOne;
+        freeCells = 9;
+        winnerClaimed = false;
+
+        gameBoard.render();
+    }
+
+    function restart() {
+        for (let i = 0; i < gameBoard.board.length; i++) {
+            gameBoard.board[i] = '';
+        }
+
+        while(gameBoard.gameField.hasChildNodes()){
+            gameBoard.gameField.removeChild(gameBoard.gameField.lastChild);
+        }    
+
+        start();
+    }
 
     const winnigPattern = [
         [0, 1, 2],
@@ -26,8 +47,6 @@ const game =  (() => {
         [0, 4, 8],
     ];
 
-    const winnerHeading = document.querySelector('.winHead');
-
     function checkWinner () {
         winnigPattern.forEach(pattern => {
             if (gameBoard.board[pattern[0]] === '' ||
@@ -36,11 +55,11 @@ const game =  (() => {
                     return;
                 }
 
-            if (gameBoard.board[pattern[0]]  === this.currentPlayer.marker &&
-                gameBoard.board[pattern[1]] === this.currentPlayer.marker &&
-                gameBoard.board[pattern[2]]  === this.currentPlayer.marker) {
-                    winnerHeading.textContent = `The winner is ${this.currentPlayer.playerName}`;
-                    this.winnerClaimed = true;
+            if (gameBoard.board[pattern[0]]  === currentPlayer.marker &&
+                gameBoard.board[pattern[1]] === currentPlayer.marker &&
+                gameBoard.board[pattern[2]]  === currentPlayer.marker) {
+                    winnerHeading.textContent = `The winner is ${currentPlayer.playerName}`;
+                    winnerClaimed = true;
                     markWinLine(pattern);
                 }
         })
@@ -48,8 +67,9 @@ const game =  (() => {
 
     function markWinLine (pattern) {
         let ind;
+        let cells = document.querySelectorAll('.cell');
         for (let i = 0; i < pattern.length; i++) {
-            gameBoard.cells.forEach(elem => {
+            cells.forEach(elem => {
                 ind = elem.getAttribute('data-ind');
                 if (ind == pattern[i]) {
                     elem.style.background = '#90EE90';
@@ -58,93 +78,98 @@ const game =  (() => {
         }
     }
 
-
-    function botPlay () {
-
-    }
-
     function switchPlayer () {
-        this.currentPlayer === playerOne ? this.currentPlayer = playerTwo : this.currentPlayer = playerOne;
-        gameBoard.player.textContent === '1st Player\'s turn' ? gameBoard.player.textContent = '2nd Player\s turn' : 
-            gameBoard.player.textContent = '1st Player\'s turn';
-
-        gameBoard.playerMark.textContent === 'marker: X' ? gameBoard.playerMark.textContent = 'marker: O': 
-            gameBoard.playerMark.textContent = 'marker: X';
+        currentPlayer === playerOne ? currentPlayer = playerTwo : currentPlayer = playerOne;
+        player.textContent = `${currentPlayer.playerName}, it's your turn!`;
+        playerMark.textContent = `Your mark: ${currentPlayer.marker}`;
     }
 
     function claimTie () {
         winnerHeading.textContent = 'That\' a tie';
+        winnerClaimed = true;
     }
+
+    function handleCLick(event) {
+        let ind = event.target.getAttribute('data-ind');
+
+        event.target.textContent = currentPlayer.marker;
+        gameBoard.board[ind] = currentPlayer.marker;
+        freeCells--;
+
+        event.target.removeEventListener('click', handleCLick);
+        checkWinner();
+
+        if (winnerClaimed){
+            currentPlayer.marker = '';
+        }
+
+        if (!winnerClaimed) {
+            if (freeCells > 0){
+                switchPlayer();
+            }
+            else if (freeCells == 0){
+                claimTie();
+            }    
+        }
+    }
+
     return {
-        currentPlayer,
-        freeCells,
-        winnerClaimed,
         checkWinner,
         switchPlayer,
         claimTie,
+        start,
+        currentPlayer,
+        freeCells,
+        winnerClaimed,
+        handleCLick,
+        restart,
     }
 })();
 
 
 const gameBoard = (() => {
     const gameField = document.querySelector('.field');
-    const player = document.querySelector('.player');
-    const playerMark = document.querySelector('.mark');
-    player.textContent = '1st Player\'s turn';
-    playerMark.textContent = 'marker: X';
 
     let board = [];
     for (let i = 0; i < 9; i++){
         board.push('');
     }
 
-    /*board.forEach(elem => {
-        console.log(elem.indexOf());
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.setAttribute('data-ind', 1);
-        gameField.appendChild(cell);
-    })*/
-
-    for (let i = 0; i < board.length; i++){
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.setAttribute('data-ind', i);
-        gameField.appendChild(cell);
-
-    }
-
-    let cells = document.querySelectorAll('.cell');
+    function render() {
     
+        board.forEach((cell, index) => {
+            cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.setAttribute('data-ind', index);
+            gameField.appendChild(cell);
+        })
 
-    for (let i = 0; i < cells.length; i++){
-        cells[i].addEventListener('click', function onClick () {
-            cells[i].textContent = game.currentPlayer.marker;
-            board[i] = game.currentPlayer.marker;
-            game.freeCells--;
-            
-            cells[i].removeEventListener('click', onClick);
-            game.checkWinner();
-            console.log(game.winnerClaimed);
-
-            if (game.winnerClaimed){
-                game.currentPlayer.marker = '';
-                
-            }
-
-            if (!game.winnerClaimed) {
-                if (game.freeCells > 0){
-                    game.switchPlayer();
-                }
-                else if (game.freeCells == 0){
-                    game.claimTie();
-                }    
-            }
-            console.log(board);
+        const cells = document.querySelectorAll('.cell');
+    
+        cells.forEach(cell => {
+            cell.addEventListener('click', game.handleCLick);
         })
     }
-    return { player, playerMark, cells, board };
+
+    return {
+        board,
+        render,
+        gameField,
+    };
 })();
+
+
+const btnStart = document.querySelector('.hi');
+btnStart.addEventListener('click', handleStart);
+function handleStart (e) {
+    game.start();
+    e.target.removeEventListener('click', handleStart);
+}
+
+const btnRestart = document.querySelector('.rr')
+    .addEventListener('click', () => {
+        game.restart();
+})
 
 
 /*const game =  (() => {
